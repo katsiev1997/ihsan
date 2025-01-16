915bdd8c18a0554899345b4626b5a08af7622f59
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2025-01-16 22:04:14 +0300

    add contact me

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { useClickOutside } from "@/shared/hooks/use-click-outside";
 
 export const Header = () => {
     const [open, setOpen] = useState(false);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    useClickOutside(ref as React.RefObject<HTMLElement>, () => setOpen(false));
+
     return (
         <>
             <header className="w-full flex items-center justify-between max-w-[1300px] px-5 mx-auto h-[105px]">
@@ -50,8 +55,62 @@ export const Header = () => {
                 </div>
             </div>
             {open && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 blur-sm flex items-center justify-center z-10">
-                    <div className="flex items-center justify-center bg-white rounded-[16px] border-[3px] w-[440px] h-[245px]"></div>
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10">
+                    <div
+                        ref={ref}
+                        className="flex items-center justify-center bg-white rounded-[16px] border-[3px] border-[#346C1E] w-[440px] h-[245px] z-20"
+                    >
+                        <div className="flex flex-col gap-3">
+                            <h3 className="text-[#346C1E] text-4xl font-semibold">
+                                СВЯЗАТЬСЯ С НАМИ
+                            </h3>
+
+                            <div className="flex items-center gap-3">
+                                <div className="flex items-center gap-1">
+                                    <a target="_blank" href="https://wa.me/+79640279097">
+                                        <Image
+                                            src="/whatsapp.png"
+                                            alt="whatsapp"
+                                            width={31}
+                                            height={31}
+                                        />
+                                    </a>
+                                    <a target="_blank" href="tel:+79640279097">
+                                        <Image
+                                            src="/phone.png"
+                                            alt="phone"
+                                            width={31}
+                                            height={31}
+                                        />
+                                    </a>
+                                </div>
+
+                                <p className="text-xl">8(964) 027-90-97</p>
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <div className="flex items-center gap-1">
+                                    <a target="_blank" href="https://wa.me/+79281113333">
+                                        <Image
+                                            src="/whatsapp.png"
+                                            alt="whatsapp"
+                                            width={31}
+                                            height={31}
+                                        />
+                                    </a>
+                                    <a target="_blank" href="tel:+79640279097">
+                                        <Image
+                                            src="/phone.png"
+                                            alt="phone"
+                                            width={31}
+                                            height={31}
+                                        />
+                                    </a>
+                                </div>
+
+                                <p className="text-xl">8(928) 111-33-33</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )}
         </>
