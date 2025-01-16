ba7e9cb695932660a1b14cff8d1c0918ee1d1f9d
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2025-01-16 22:24:21 +0300

    change style card

--- src/shared/ui/card.tsx
+++ src/shared/ui/card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 
 type Props = {
@@ -11,31 +11,30 @@ type Props = {
 
 export const Card = ({ imageUrl, title, alt }: Props) => {
     const [open, setOpen] = useState(false);
-    const contentRef = useRef<HTMLDivElement>(null);
+
     return (
         <div
             onMouseEnter={() => setOpen(true)}
             onMouseLeave={() => setOpen(false)}
-            className="w-full px-5 border-2 border-[#346C1E] rounded-[6px] bg-white flex flex-col justify-center items-center snap-center relative"
+            className="w-full px-5 border-2 border-[#346C1E] rounded-[6px] bg-white flex flex-col justify-center items-center snap-center"
         >
-            <div className="w-[396px] h-[361px] p-5 flex flex-col items-center gap-3">
-                <Image src={imageUrl} alt={alt} width={329} height={264} />
-                <div className="h-[1px] w-[120px] bg-[#346C1E]" />
-                <h5 className="font-bold text-[22px] text-[#346C1E]">{title}</h5>
-            </div>
-            <div
-                ref={contentRef}
-                className="w-full transition-[max-height] duration-300 ease-in-out overflow-hidden"
-                style={{
-                    maxHeight: open ? `${contentRef.current?.scrollHeight}px` : "0",
-                }}
-            >
-                <p className="text-[#346c1e] text-xl">
-                    Мы оказываем адресную помощь малоимущим семьям, детям-сиротам, инвалидам,
-                    больным, нуждающимся в лечении, и другим категориям граждан, оказавшимся в
-                    трудной жизненной ситуации.
-                </p>
-            </div>
+            {!open ? (
+                // Блок с изображением и заголовком
+                <div className="w-[396px] h-[361px] p-5 flex flex-col items-center gap-3 transition-all">
+                    <Image src={imageUrl} alt={alt} width={329} height={264} />
+                    <div className="h-[1px] w-[120px] bg-[#346C1E]" />
+                    <h5 className="font-bold text-[22px] text-[#346C1E]">{title}</h5>
+                </div>
+            ) : (
+                // Блок с текстом
+                <div className="w-[396px] h-[361px] p-5 flex items-center justify-center transition-all">
+                    <p className="text-[#346c1e] text-xl text-center">
+                        Мы оказываем адресную помощь малоимущим семьям, детям-сиротам, инвалидам,
+                        больным, нуждающимся в лечении, и другим категориям граждан, оказавшимся в
+                        трудной жизненной ситуации.
+                    </p>
+                </div>
+            )}
         </div>
     );
 };
